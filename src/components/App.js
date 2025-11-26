181d45d114226d506901ad2d23d0b70d9f649911
pavantejareddy1 <pavantejareddy85@gmail.com>
2025-11-26 17:49:47 +0530

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import PageLayout from "./PageLayout";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <PageLayout header={<h1>Welcome to my webiste</h1>} footer={<p>2023 My Webiste. All rights reserved</p>}> 
+      <p>This is the content of my webiste </p>
+      </PageLayout>
     </div>
   )
 }
